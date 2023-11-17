195598cf31af0b86d296b6cc89dcaba325bf9400
Ayrton Filho <ayrton@Find.me>
2023-11-17 15:31:28 -0300

    imagens ok

--- imagem.py
+++ imagem.py
@@ -1,4 +1,4 @@
-import pygame  
+import pygame
 from neat.mapa.mapa import Mapa
 
 class Imagem():
@@ -6,56 +6,50 @@ class Imagem():
         self.x = x_mapa
         self.y = y_mapa
         self.mapa = mapa
-        
+
     def imagem(self):
-        
         pygame.init()
         # Configurações da tela
-        screen_width = 1900
-        screen_height = 1000
+        screen_width = 500
+        screen_height = 400
         screen = pygame.display.set_mode((screen_width, screen_height))
+        
+        # Carregar imagens
+        grama_image = pygame.image.load('img/floor.png')
+        grama_image = pygame.transform.scale(grama_image, (int(screen_width / self.x), int(screen_height / self.y)))
+
+        earth_image = pygame.image.load('img/earth.png')
+        earth_image = pygame.transform.scale(earth_image, (int(screen_width / self.x), int(screen_height / self.y)))
+
+        rock_image = pygame.image.load('img/rock.png')
+        rock_image = pygame.transform.scale(rock_image, (int(screen_width / self.x), int(screen_height / self.y)))
 
         # Cores
-        BLACK = (0, 0, 0) # Teoricamente é pra ser a área livre mas não sei se tá sendo
-        RED = (255, 0, 0)   # Predador
-        GREEN = (0, 255, 0) # Presa
-        BLUE = (0, 0, 255)  # Livre
-
-
-        # Inicializa o mapa e a simulação
+        Water = (0, 0, 255)  # Cor azul água
 
         # Loop principal
         running = True
-
         while running:
-            # Processa eventos
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
 
-            # Limpar tela
-            # screen.fill(BLACK)
-
-
-            # Renderiza o mapa e obstáculos
             escala_x = screen_width / self.x
             escala_y = screen_height / self.y
+
             for y in range(self.y):
                 for x in range(self.x):
                     tipo_terreno = self.mapa[y][x]
-                    if tipo_terreno == 1:  # Exemplo: 1 representa um obstáculo
-                        pygame.draw.rect(screen, BLUE, (x * escala_x, y * escala_y, escala_x, escala_y)) # Obstáculo em azul
-                    elif tipo_terreno ==  0:
-                        pygame.draw.rect(screen, BLACK, (x * escala_x, y * escala_y, escala_x, escala_y))
-                    elif tipo_terreno == 2:
-                        pygame.draw.ellipse(screen, GREEN, (x * escala_x, y * escala_y, escala_x, escala_y))
-                    # elif tipo_terreno == 3:
-                    #     pygame.draw.circle(screen, RED, )
+                    if tipo_terreno == 1:  # Obstáculo
+                        screen.blit(rock_image, (x * escala_x, y * escala_y))
+                    elif tipo_terreno == 0:  # Espaço livre
+                        screen.blit(earth_image, (x * escala_x, y * escala_y))
+                    elif tipo_terreno == 2:  # Grama
+                        screen.blit(grama_image, (x * escala_x, y * escala_y))
+                    elif tipo_terreno == 3:  # Água
+                        pygame.draw.rect(screen, Water, (x * escala_x, y * escala_y, escala_x, escala_y))
 
-            pygame.display.flip()
-            pygame.time.delay(2000)  # 2 segundos
-            
-
+            pygame.display.flip()  # Atualiza a tela
 
 # Terra - espaço livre = 0
 # Obstáculo = 1
@@ -64,8 +58,12 @@ class Imagem():
 # Presa = 4
 
 mapa = Mapa(4, 4)
-imagem = Imagem(4,4, mapa.mapa)
+imagem = Imagem(4, 4, mapa.mapa)
 
 mapa.printar_mapa()
 
 imagem.imagem()
+
+
+
+# PROBLEMAS: O ANIMAL PODE FICAR PRESO CERCADO POR PEDRA - CONSERTAR ISSO EM MAPA
